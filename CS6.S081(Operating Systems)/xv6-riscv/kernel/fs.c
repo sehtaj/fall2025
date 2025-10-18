c0f8542a6f77697c08568ca04b6492536b003301
Sehtaj Singh <batthsehtaj10@gmail.com>
2025-10-18 19:09:17 +0530

    AS8 lab-fs Solved Q1

--- CS6.S081(Operating Systems)/xv6-riscv/kernel/fs.c
+++ CS6.S081(Operating Systems)/xv6-riscv/kernel/fs.c
@@ -382,8 +382,8 @@ iunlockput(struct inode *ip)
 static uint
 bmap(struct inode *ip, uint bn)
 {
-  uint addr, *a;
-  struct buf *bp;
+  uint addr, *a, *a2;
+  struct buf *bp, *bp2;
 
   if(bn < NDIRECT){
     if((addr = ip->addrs[bn]) == 0){
@@ -416,32 +416,88 @@ bmap(struct inode *ip, uint bn)
     brelse(bp);
     return addr;
   }
+  	
+  // Double indirect
+  bn -= NINDIRECT;
+  uint dbl_index = bn / NINDIRECT;
+  uint ind_index = bn % NINDIRECT;
+  if(dbl_index >= NINDIRECT)
+    panic("bmap: out of range");
 
-  panic("bmap: out of range");
-}
+  if((addr = ip->addrs[NDIRECT + 1]) == 0){
+    addr = balloc(ip->dev);
+    if(addr == 0)
+      return 0;
+    ip->addrs[NDIRECT + 1] = addr;
+  }
+  bp = bread(ip->dev, addr);
+  a = (uint*)bp->data;
+  if((addr = a[dbl_index]) == 0){
+    addr = balloc(ip->dev);
+    if(addr == 0){
+      brelse(bp);
+      return 0;
+    }
+    a[dbl_index] = addr;
+    log_write(bp);
+  }
+  bp2 = bread(ip->dev, addr);
+  a2 = (uint*)bp2->data;
+  if((addr = a2[ind_index]) == 0){
+    addr = balloc(ip->dev);
+    if(addr){
+      a2[ind_index] = addr;
+      log_write(bp2);
+    }
+  }
+  brelse(bp2);
+  brelse(bp);
+  return addr;
+  
+  }
 
 // Truncate inode (discard contents).
 // Caller must hold ip->lock.
 void
 itrunc(struct inode *ip)
 {
-  int i, j;
-  struct buf *bp;
-  uint *a;
+  int i, j, k;
+  struct buf *bp, *bp2;
+  uint *a, *a2;
 
-  for(i = 0; i < NDIRECT; i++){
+  for(i = 0; i < NDIRECT - 1; i++){
     if(ip->addrs[i]){
       bfree(ip->dev, ip->addrs[i]);
       ip->addrs[i] = 0;
     }
   }
 
-  if(ip->addrs[NDIRECT]){
-    bp = bread(ip->dev, ip->addrs[NDIRECT]);
+  if(ip->addrs[NDIRECT - 1]){
+    bp = bread(ip->dev, ip->addrs[NDIRECT - 1]);
     a = (uint*)bp->data;
     for(j = 0; j < NINDIRECT; j++){
       if(a[j])
         bfree(ip->dev, a[j]);
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT - 1]);
+    ip->addrs[NDIRECT - 1] = 0;
+  }
+    // Free doubly-indirect blocks
+  if(ip->addrs[NDIRECT]){
+    bp = bread(ip->dev, ip->addrs[NDIRECT]);
+    a = (uint*)bp->data;
+    for(j = 0; j < NINDIRECT; j++){
+      if(a[j]){
+        bp2 = bread(ip->dev, a[j]);
+        a2 = (uint*)bp2->data;
+        for(k = 0; k < NINDIRECT; k++){
+          if(a2[k])
+            bfree(ip->dev, a2[k]);
+        }
+        brelse(bp2);
+        bfree(ip->dev, a[j]);
+      }
     }
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
